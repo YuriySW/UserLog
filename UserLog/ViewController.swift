e3cc8cf685b0dc4d16746eb2f2e4d50d9ef60f2a
YuriySW <yuriy.s@my.com>
2021-10-27 00:50:05 +0500

    bad

--- UserLog/ViewController.swift
+++ UserLog/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var passwordText: UITextField!
+    @IBOutlet var userName: UITextField!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        }
+   
+    @IBAction func login(_ sender: Any) {
+        if userName.text == "User" && passwordText.text == "Password"{
+            performSegue(withIdentifier: "go", sender: nil)
+        }
     }
-
-
+    
+    
+    @IBAction func forgotName(_ sender: Any) {
+        showAlert(title: "Your login", message: "User")
+    }
+    
+    @IBAction func forgotPass(_ sender: Any) {
+        showAlert(title: "Your password", message: "Password")
+    }
+   
 }
 
+
+
+
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) {_ in}
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  UserLog
+//
+//  Created by Yuriy  on 26.10.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+}
